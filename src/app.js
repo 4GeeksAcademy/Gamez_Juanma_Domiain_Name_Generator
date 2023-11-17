8c3b986cb1541c5651904d87a011d229e6673f0f
JuanmaGF <137336081+JuanmaGF@users.noreply.github.com>
2023-11-17 20:03:56 +0000

    todo menos domain hacks

--- src/app.js
+++ src/app.js
@@ -7,5 +7,20 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let extension = [".es", ".com", ".net", ".us", ".io"];
+  function domainNameGenerator(arr1, arr2, arr3, arr4) {
+    for (let i = 0; i < arr1.length; i++) {
+      for (let j = 0; j < arr2.length; j++) {
+        for (let x = 0; x < arr3.length; x++) {
+          for (let y = 0; y < arr4.length; y++) {
+            console.log(arr1[i] + arr2[j] + arr3[x] + arr4[y]);
+          }
+        }
+      }
+    }
+  }
+  console.log(domainNameGenerator(pronoun, adj, noun, extension));
 };
